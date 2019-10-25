170e5100b7e369465d6ea69cfe06d6f6ef8713d6
Sanchit Dass <sanchitd9@gmail.com>
2019-10-26 03:28:23 +0530

    Need to fix duplicate username bug

--- .flaskenv
+++ .flaskenv
@@ -1,1 +1,6 @@
 export FLASK_ENV=microblog.py
+export MAIL_SERVER=smtp.googlemail.com
+export MAIL_PORT=587
+export MAIL_USE_TLS=1
+export MAIL_USERNAME=sanchitdass97@gmail.com
+export MAIL_PASSWORD=shaan@97
--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from werkzeug import generate_password_hash, check_password_hash
 from app import db, login
 from datetime import datetime
+from hashlib import md5
 from flask_login import UserMixin
 
 
@@ -14,6 +15,8 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index = True, unique = True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref = 'author', lazy = 'dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default = datetime.utcnow)
 
 
     def __repr__(self):
@@ -23,7 +26,11 @@ class User(UserMixin, db.Model):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
-        return check_password_hash(self.password_hash, password) 
+        return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size) 
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key = True)
